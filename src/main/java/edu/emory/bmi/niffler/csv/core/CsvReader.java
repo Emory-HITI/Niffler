719ce37a24fd68f24b2a6b9272be3bcd3ad21f55
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2020-07-15 19:07:37 -0400

    Update CsvReader.java
    Add csv header

--- src/main/java/edu/emory/bmi/niffler/csv/core/CsvReader.java
+++ src/main/java/edu/emory/bmi/niffler/csv/core/CsvReader.java
@@ -52,6 +52,7 @@ public class CsvReader {
             for (File file: listOfFiles) {
                 try {
                     String fileStr = file.toString();
+                    ScannerSingleton.next(fileStr);
                     Path path = Paths.get(fileStr);
                     List<AbstractCsvBean> out = convertToBean(path, IntermediaryCsvBean.class);
                     ScannerUtil.getFinalCsvString(out, file.getName(), scannersSubsetMap);
