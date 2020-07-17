554b33a06df642f093781ccce390e140be49ab98
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2020-07-16 23:26:41 -0400

    Fix script name

---
+++ src/scripts/month-util.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+declare -i base=20200600
+for (( i=1; i <= 30; i++ ))
+do
+    base=$base+1
+    filename="$base.csv"
+    mongo --quiet --eval "var param1='$base'" scanner-util.js > $filename
+done
+
