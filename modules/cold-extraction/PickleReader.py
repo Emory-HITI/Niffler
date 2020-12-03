c49645644e993116882ace71a6874d08ecd2e422
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2020-12-03 11:04:07 -0500

    Remove redundant lines

--- modules/cold-extraction/PickleReader.py
+++ modules/cold-extraction/PickleReader.py
@@ -16,18 +16,7 @@ logging.basicConfig(filename=niffler_log,level=logging.INFO, format='%(message)s
 # Variables to track progress between iterations.
 extracted_ones = list()
 
-# By default, assume that this is a fresh extraction.
-resume = False
-
-# All extracted files from the csv file are saved in a respective .pickle file.
-
 with open(csv_file +'.pickle', 'rb') as f:
     extracted_ones = pickle.load(f)
-    wrap_start = '['
-    wrap_end = ']'
-    # temp = extracted_ones.partition(wrap_start)[2] 
-    # output = temp.partition(wrap_end)[1]
     for i in extracted_ones:
         logging.info(i.replace("_", ", "))
-
-    # Since we have successfully located a pickle file, it indicates that this is a resume.
