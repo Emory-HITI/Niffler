aab8cff45d7ca30a11b0388ac0cebbae435155b3
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2021-02-01 20:11:52 -0500

    Add a split csv file functionality

---
+++ modules/cold-extraction/SplitCSV.py
@@ -0,0 +1,33 @@
+import os
+import csv
+
+def split(filehandler, delimiter=',', row_limit=4276,
+          output_name_template='output_%s.csv', output_path='.', keep_headers=True):
+    reader = csv.reader(filehandler, delimiter=delimiter)
+    
+
+    current_piece = 1
+    current_out_path = os.path.join(
+        output_path,
+        output_name_template % current_piece
+    )
+    current_out_writer = csv.writer(open(current_out_path, 'w'), delimiter=delimiter)
+    current_limit = row_limit
+    if keep_headers:
+        headers = next(reader)
+        current_out_writer.writerow(headers)
+    for i, row in enumerate(reader):
+        if i + 1 > current_limit:
+            current_piece += 1
+            current_limit = row_limit * current_piece
+            current_out_path = os.path.join(
+                output_path,
+                output_name_template % current_piece
+            )
+            current_out_writer = csv.writer(open(current_out_path, 'w'), delimiter=delimiter)
+            if keep_headers:
+                current_out_writer.writerow(headers)
+        current_out_writer.writerow(row)
+
+
+split(open('origin.csv', 'r'));
