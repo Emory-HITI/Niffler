257673594082edc2f91de8091017510d1b611d0d
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2021-02-01 22:39:36 -0500

    Refactor SplitCSV

--- modules/cold-extraction/SplitCSV.py
+++ modules/cold-extraction/SplitCSV.py
@@ -1,18 +1,21 @@
 import os
 import csv
 
-row_limit = 0
 file_name = 'origin.csv'
+output_name_template='output_%s.csv'
+output_path='.'
+keep_headers=True
 
-def split(delimiter=',', output_name_template='output_%s.csv', output_path='.', keep_headers=True):
+row_limit = 0
+delimiter = ','
+
+
+def split():
     filehandler = open(file_name, 'r')
     reader = csv.reader(filehandler, delimiter=delimiter)
 
     current_piece = 1
-    current_out_path = os.path.join(
-        output_path,
-        output_name_template % current_piece
-    )
+    current_out_path = os.path.join(output_path, output_name_template % current_piece)
     current_out_writer = csv.writer(open(current_out_path, 'w'), delimiter=delimiter)
     current_limit = row_limit
     if keep_headers:
@@ -22,10 +25,7 @@ def split(delimiter=',', output_name_template='output_%s.csv', output_path='.', 
         if i + 1 > current_limit:
             current_piece += 1
             current_limit = row_limit * current_piece
-            current_out_path = os.path.join(
-                output_path,
-                output_name_template % current_piece
-            )
+            current_out_path = os.path.join(output_path, output_name_template % current_piece)
             current_out_writer = csv.writer(open(current_out_path, 'w'), delimiter=delimiter)
             if keep_headers:
                 current_out_writer.writerow(headers)
@@ -33,7 +33,7 @@ def split(delimiter=',', output_name_template='output_%s.csv', output_path='.', 
 
 
 with open(file_name,'r') as f:
-    reader = csv.reader(f,delimiter = ",")
+    reader = csv.reader(f)
     data = list(reader)
     row_limit = int(len(data) / 2)
 
