c7330196e267cc8731b60bbe0004ada3cc45cc13
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2021-03-01 21:55:30 -0500

    Remove hard-coded path

--- modules/meta-extraction/service/mdextractor.sh
+++ modules/meta-extraction/service/mdextractor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nohup python3.6 -u /opt/localdrive/Niffler/modules/meta-extraction/MetadataExtractor.py >> /opt/localdrive/Niffler/modules/meta-extraction/py.out &
+nohup python3.6 -u ../MetadataExtractor.py >> py.out &
 wait
-echo "The Researchpacs Metadata Extractor Process has failed" >> /opt/localdrive/Niffler/modules/meta-extraction/py.out
+echo "The Researchpacs Metadata Extractor Process has failed" >> py.out
 echo "The Researchpacs Metadata Extractor Process has failed" | mail -s "The Researchpacs Metadata Extractor Process has failed" pkathi2@emory.edu
