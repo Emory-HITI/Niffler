c1334eb630d31df8f57285dc60560f8f7442f02e
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2021-03-01 22:35:53 -0500

    Update mdextractor.sh

--- modules/meta-extraction/service/mdextractor.sh
+++ modules/meta-extraction/service/mdextractor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 nohup python3.6 -u ../MetadataExtractor.py >> niffler-rt.out &
 wait
-echo "The Researchpacs Metadata Extractor Process has failed" >> niffler-rt.out
-echo "The Researchpacs Metadata Extractor Process has failed" | mail -s "The Researchpacs Metadata Extractor Process has failed" pkathi2@emory.edu
+echo "The Niffler Metadata Extractor Process has failed" >> niffler-rt.out
+echo "The Niffler Metadata Extractor Process has failed" | mail -s "The Niffler Metadata Extractor Process has failed" pkathi2@emory.edu
