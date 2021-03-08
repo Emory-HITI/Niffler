b6eb989ae745fb83d104cb8de3ec2413eb415580
pradeeban <kk.pradeeban@gmail.com>
2021-03-08 16:54:49 -0500

    Add conda dependency

--- install.sh
+++ install.sh
@@ -6,8 +6,14 @@ if [ "$PIP" = false ] ; then
     echo "Installing pip"
     sudo yum install python3-pip
     sudo pip3 install -r requirements.txt
+    wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
+    sh Anaconda3-2020.11-Linux-x86_64.sh -u
+    source ~/.bashrc
+    rm Anaconda3-2020.11-Linux-x86_64.sh
     echo "true" > init/pip.out
 fi
+
+conda install -c conda-forge -y gdcm
 
 DCM4CHE=`head -n 1 init/dcm4che.out`
 if [ "$DCM4CHE" = false ] ; then
@@ -15,6 +21,7 @@ if [ "$DCM4CHE" = false ] ; then
     cd ..
     wget https://sourceforge.net/projects/dcm4che/files/dcm4che3/5.22.5/dcm4che-5.22.5-bin.zip/download -O dcm4che-5.22.5-bin.zip
     unzip dcm4che-5.22.5-bin.zip
+    rm dcm4che-5.22.5-bin.zip
     cd Niffler
     echo "true" > init/dcm4che.out
 fi
