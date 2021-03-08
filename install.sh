c90d3860f1af7661a9d107dd7d6602f1f5d7b3c2
pradeeban <kk.pradeeban@gmail.com>
2021-03-08 14:34:56 -0500

    Configure pip

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ echo "Installing pip"
 sudo yum install python3-pip
 alias python=python3
 alias pip=pip3
-echo "Installing mongo"
 MONGO=`head -n 1 config/mongo.out`
-if [ "$MONGO" = true ] ; then
+if [ "$MONGO" = false ] ; then
+    echo "Installing mongo"
     sudo cp config/mongodb-org-4.2.repo /etc/yum.repos.d/
     sudo yum install mongodb-org
     sudo systemctl start mongod
