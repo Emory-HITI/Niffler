0dc717aecf87f02e816d477e0121c1a87df6d9af
Pradeeban Kathiravelu <pkathi2@researchpacs3.bmi.emory.edu>
2021-03-08 14:25:51 -0500

    Init script to install mongo

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
-
 #!/bin/sh
 echo "Configuring Niffler"
-
+echo "Installing pip"
+sudo yum install python3-pip
+alias python=python3
+alias pip=pip3
+echo "Installing mongo"
+MONGO=`head -n 1 config/mongo.out`
+if [ "$MONGO" = true ] ; then
+    sudo cp config/mongodb-org-4.2.repo /etc/yum.repos.d/
+    sudo yum install mongodb-org
+    sudo systemctl start mongod
+    sudo systemctl enable mongod
+    echo "true" > config/mongo.out
+fi
