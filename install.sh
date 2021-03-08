f23e0f8085c598fcca6721fc97dd34392117a4b1
pradeeban <kk.pradeeban@gmail.com>
2021-03-08 18:16:57 -0500

    Install OpenJDK and Maven

--- install.sh
+++ install.sh
@@ -17,6 +17,10 @@ conda install -c conda-forge -y gdcm
 
 DCM4CHE=`head -n 1 init/dcm4che.out`
 if [ "$DCM4CHE" = false ] ; then
+    echo "Installing JDK"
+    sudo yum install java-1.8.0-openjdk-devel
+    echo "Installing Maven"
+    sudo dnf install maven
     echo "Installing DCM4CHE"
     cd ..
     wget https://sourceforge.net/projects/dcm4che/files/dcm4che3/5.22.5/dcm4che-5.22.5-bin.zip/download -O dcm4che-5.22.5-bin.zip
