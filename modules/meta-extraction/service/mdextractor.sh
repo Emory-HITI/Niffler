477d90676f5094de4e4fb38063b9dd95e5ed07db
pradeeban <kk.pradeeban@gmail.com>
2021-03-08 19:41:11 -0500

    Fix mdextractor service

--- modules/meta-extraction/service/mdextractor.sh
+++ modules/meta-extraction/service/mdextractor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /opt/localdrive/Niffler/modules/meta-extraction/
-nohup python3.6 -u MetadataExtractor.py >> niffler-rt.out &
+nohup python -u MetadataExtractor.py >> niffler-rt.out &
 wait
 echo "The Niffler Metadata Extractor Process has failed" >> niffler-rt.out
-echo "The Niffler Metadata Extractor Process has failed" | mail -s "The Niffler Metadata Extractor Process has failed" pkathi2@emory.edu
+echo "The Niffler Metadata Extractor Process has failed" | mail -s "The Niffler Metadata Extractor Process has failed" test@test.test
