965c493769e5605572470077d082609d655d8a63
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2021-07-20 19:07:50 -0400

    Fix filepath typo

--- modules/cold-extraction/ColdDataRetriever.py
+++ modules/cold-extraction/ColdDataRetriever.py
@@ -238,7 +238,7 @@ def retrieve():
             if file_path == "CFIND-ONLY":
                 all_files = glob.glob(os.path.join(temp_folder, "*.csv"))
                 df_from_each_file = (pd.read_csv(f, sep=',') for f in all_files)
-                df_merged = pd.concat(df_from_each_file, ignore_index=True)
+                df_merged = pd.concat(df_from_each_file, ignore_index=False)
                 df_merged.to_csv(storage_folder + "/cfind-output.csv")
                 shutil.rmtree(temp_folder)
 
