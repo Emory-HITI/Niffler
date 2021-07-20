ef0c9e0c85d7919344a24afccbb52a504a5190a6
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2021-07-20 19:14:53 -0400

    Fix csv merge

--- modules/cold-extraction/ColdDataRetriever.py
+++ modules/cold-extraction/ColdDataRetriever.py
@@ -238,6 +238,7 @@ def retrieve():
             if file_path == "CFIND-ONLY":
                 cwd = os.getcwd()
                 extension = 'csv'
+                os.chdir(temp_folder)
                 all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
                 # combine all files in the list
                 combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
