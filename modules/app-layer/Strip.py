e1c0a23f81693a7e8af818a88391f90dcc8e1ee4
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2021-09-23 13:14:17 -0400

    Add a config file

--- modules/app-layer/Strip.py
+++ modules/app-layer/Strip.py
@@ -1,19 +1,27 @@
 import pandas
 import logging
+import json
 
 logging.basicConfig(level=logging.INFO)
-feature_file = 'properties.txt'
 
 
-def initialize_config_and_execute(filename):
+def initialize_config_and_execute():
+    # Reads the system_json file.
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+
+    feature_file = config['FeatureFile']
+    filename = config['InputFile']
+    output_csv = config['OutputFile']
+
     text_file = open(feature_file, "r")
     feature_list = text_file.read().split('\n')
 
     filtered_csv = pandas.read_csv(filename, usecols=lambda x: x in feature_list, sep=',')
     logging.info(filtered_csv)
     df = pandas.DataFrame(filtered_csv)
-    df.to_csv()
+    df.to_csv(output_csv)
 
 
 if __name__ == "__main__":
-    initialize_config_and_execute('sample.csv')
+    initialize_config_and_execute()
