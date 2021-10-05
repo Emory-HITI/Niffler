1883e1b7018f94690a078b843aa7de508d1affbb
Ananth Reddy <ananthreddy@Ananths-MacBook-Air.local>
2021-10-05 10:45:24 -0400

    Added functionality for meds and orders data. Included logging info for tracking outliers in the data.

--- modules/rta-extraction/RtaExtractor.py
+++ modules/rta-extraction/RtaExtractor.py
@@ -16,6 +16,7 @@ import glob
 from warnings import resetwarnings
 from bson.objectid import ObjectId
 import pymongo
+from pymongo import database
 from pymongo.message import delete, query
 import pydicom
 import requests
@@ -35,13 +36,11 @@ from datetime import datetime, timedelta
 from pymongo import MongoClient
 from collections import Counter
 
-def run_threaded(job_func):
-    job_thread = threading.Thread(target=job_func)
-    job_thread.start()
-
 # Data Loading Function
 def load_json_data(url, user, passcode, db_json=None, first_index=None, second_index=None):
-
+    '''
+    Loads the json data from labs, meds and orders into corresponsing MongoDB Collection.
+    '''
     # Parameters:
     # 1. url          - URL to get the data.
     # 2. user         - Username for Authorization of ResearchPACS Server.
@@ -50,6 +49,7 @@ def load_json_data(url, user, passcode, db_json=None, first_index=None, second_i
     # 5. first_index  - First index in the MongoDB Collection. (Usually a date time attribute)
     # 6. second_index - Second index in the MongoDB Collection. (Usually empi information attribute)
 
+    global total_data
     load_time = time.time()
     data_collection = db[db_json]
     data = requests.get(url, auth=(user, passcode))
@@ -57,13 +57,17 @@ def load_json_data(url, user, passcode, db_json=None, first_index=None, second_i
     items_data = data['items']
 
     for record in items_data:
-        data_collection.insert_one(record)
-        data_collection.create_index(
-            [
-                (first_index, 1),
-                (second_index, 1)
-            ]
-        )
+        if record not in total_data:
+            data_collection.insert_one(record)
+            total_data.append(record)
+            data_collection.create_index(
+                [
+                    (first_index, 1),
+                    (second_index, 1)
+                ]
+            )
+    logging.info(len(total_data))
+    view_data(db_json)
 
     for i in data['links']:
         if (i['rel'] == 'next'):
@@ -75,19 +79,28 @@ def load_json_data(url, user, passcode, db_json=None, first_index=None, second_i
 
 # Data Clearing Function
 def clear_data(db_json=None):
-
-    # Parameters
+    '''
+    Clears the data which is older than one day from MongoDB Collection.
+    '''
+    # Parameters:
     # 1. db_json - Name of the MongoDB Collection.
 
     clear_time = time.time()
     data_collection = db[db_json]
     cursor = data_collection.find({})
 
+    if db_json == 'labs_json':
+        date_column = 'lab_date'
+    elif db_json == 'meds_json':
+        date_column = 'update_dt_tm'
+    elif db_json == 'orders_json':
+        date_column = 'completed_dt_tm'
+
     for document in cursor:
         previous_time = datetime.now()-timedelta(days=1)
         previous_date = previous_time.date()
 
-        item_date = datetime.strptime(document['lab_date'], '%Y-%m-%dT%H:%M:%SZ').date()
+        item_date = datetime.strptime(document[date_column], '%Y-%m-%dT%H:%M:%SZ').date()
         diff_time = previous_date-item_date
 
         if (diff_time.total_seconds()>=0):
@@ -98,6 +111,13 @@ def clear_data(db_json=None):
     
 # Data Filtering Function
 def view_data(db_json=None, user_query=None):
+    '''
+    Display the shape, outliers and value counts of the dataframe.
+    '''
+    # Parameters:
+    # 1. db_json    - Name of the MongoDB Collection.
+    # 2. user_query - List of features.
+
     view_time = time.time()
     data_collection = db[db_json]
     data_cursor = data_collection.find({})
@@ -105,13 +125,76 @@ def view_data(db_json=None, user_query=None):
     doc_list = []
     for document in data_cursor:
         doc_list.append(document)
+
+    if db_json == 'labs_json':
+        date_column = 'lab_date'
+    elif db_json == 'meds_json':
+        date_column = 'update_dt_tm'
+    elif db_json == 'orders_json':
+        date_column = 'completed_dt_tm'
         
     df = pd.DataFrame(doc_list)
-    logging.info (df.shape)
+    df[date_column] = pd.to_datetime(df[date_column].str.split('T').str[0])
+    logging.info(df.shape)
+    logging.info(str(df[date_column].value_counts().to_dict()))
+
+    logging.info('Outliers - {}'.format(df[date_column][datetime.now()-df[date_column] > timedelta(30)].shape[0]))
+    logging.info('Outliers Percentage - {}'.format(df[date_column][datetime.now()-df[date_column] > timedelta(30)].shape[0]/df[date_column].shape[0]))
 
     time_taken = round(time.time()-view_time, 2)
     logging.info('Spent {} seconds viewing the data of {}.'.format(time_taken, db_json))
 
+def load_labs_data():
+    '''
+    A buffer function between main and load_json_data functions for labs data.
+    '''
+    global total_data
+    global UserName
+    global PassCode
+
+    now = datetime.now()
+    logging.info('Labs Data')
+    logging.info(now.strftime('%Y-%m-%d %H:%M:%S'))
+
+    load_json_data(url=LabsURL, user=UserName, passcode=PassCode, db_json='labs_json', first_index='lab_date', 
+                   second_index='empi')
+
+
+def load_meds_data():
+    '''
+    A buffer function between main and load_json_data functions for meds data.
+    '''
+    global total_data
+    global UserName
+    global PassCode
+
+    now = datetime.now()
+    logging.info('Meds Data')
+    logging.info(now.strftime('%Y-%m-%d %H:%M:%S'))
+
+    load_json_data(url=MedsURL, user=UserName, passcode=PassCode, db_json='meds_json', first_index='update_dt_tm', 
+                   second_index='empi')
+
+
+def load_orders_data():
+    '''
+    A buffer function between main and load_json_data functions for orders data.
+    '''
+    global total_data
+    global UserName
+    global PassCode
+
+    now = datetime.now()
+    logging.info('Orders Data')
+    logging.info(now.strftime('%Y-%m-%d %H:%M:%S'))
+    
+    load_json_data(url=OrdersURL, user=UserName, passcode=PassCode, db_json='orders_json', first_index='completed_dt_tm', 
+                   second_index='empi')
+
+
+def run_threaded(job_func):
+    job_thread = threading.Thread(target=job_func)
+    job_thread.start()
 
 if __name__ == "__main__":
     log_format = '%(levelname)s %(asctime)s - %(message)s'
@@ -123,20 +206,17 @@ if __name__ == "__main__":
         niffler = json.load(f)
 
     # Get constants from system.json
-    Labs_FolderPath = niffler['LabsFilePath']
-    Meds_FolderPath = niffler['MedsFilePath']
-    Orders_FolderPath = niffler['OrdersFilePath']
     LabsURL = niffler['LabsURL']
     MedsURL = niffler['MedsURL']
     OrdersURL = niffler['OrdersURL']
     Labs_ExtractionFrequency = niffler['LabsDataExtractionFrequency']
     Meds_ExtractionFrequency = niffler['MedsDataExtractionFrequency']
     Orders_ExtractionFrequency = niffler['OrdersDataExtractionFrequency']
+    UserName = niffler['UserName']
+    PassCode = niffler['PassCode']
     Mongo_URI = niffler['MongoURI']
     Mongo_UserName = niffler['MongoUserName']
     Mongo_PassCode = niffler['MongoPassCode']
-    UserName = niffler['UserName']
-    PassCode = niffler['PassCode']
 
     # Connect to MongoDB
     connection_start_time = time.time()
@@ -149,20 +229,12 @@ if __name__ == "__main__":
 
     db = client.database
     
-    schedule.every(Labs_ExtractionFrequency).minutes.do(run_threaded, 
-                                                        load_json_data(url=LabsURL, 
-                                                        user=UserName, passcode=PassCode, db_json='labs_json', 
-                                                        first_index='lab_date', second_index='empi'))
-    schedule.every(Labs_ExtractionFrequency).minutes.do(run_threaded, 
-                                                        load_json_data(url=MedsURL, 
-                                                        user=UserName, passcode=PassCode, db_json='meds_json', 
-                                                        first_index='update_dt_tm', second_index='empi'))
-    schedule.every(Labs_ExtractionFrequency).minutes.do(run_threaded, 
-                                                        load_json_data(url=OrdersURL,
-                                                        user=UserName, passcode=PassCode, db_json='orders_json', 
-                                                        irst_index='completed_dt_tm', second_index='empi'))
+    total_data = []
+    schedule.every(Labs_ExtractionFrequency).minutes.do(run_threaded, load_labs_data())
+    schedule.every(Meds_ExtractionFrequency).minutes.do(run_threaded, load_meds_data())
+    schedule.every(Orders_ExtractionFrequency).minutes.do(run_threaded, load_orders_data())
 
-    schedule.every(1).day.at("23:59").do(run_threaded, clear_data(db_json='labs_json'))
+    schedule.every(1).day.at('23:59').do(run_threaded, clear_data(db_json='labs_json'))
     schedule.every(1).day.at("23:59").do(run_threaded, clear_data(db_json='meds_json'))
     schedule.every(1).day.at("23:59").do(run_threaded, clear_data(db_json='orders_json'))
 
