307eccc0c303a2f700a38eb43a85a60e31f522ff
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2021-12-15 10:41:58 -0500

    Add comments

--- modules/suvpar/Suvpar.py
+++ modules/suvpar/Suvpar.py
@@ -51,6 +51,8 @@ def strip():
         df.rename(columns={'max': 'SeriesEndTime'}, inplace=True)
         df['SeriesStartTime'] = pandas.to_datetime(df['SeriesStartTime'])
         df['SeriesEndTime'] = pandas.to_datetime(df['SeriesEndTime'])
+
+        # Compute series duration in minutes
         df['SeriesDurationInMins'] = (df.SeriesEndTime - df.SeriesStartTime).dt.seconds / 60.0
 
         # Keep only one instance per series. 322,866 rows drops to 3,656 in a tested sample, by this step.
@@ -65,6 +67,8 @@ def strip():
         df.rename(columns={'max': 'StudyEndTime'}, inplace=True)
         df['StudyStartTime'] = pandas.to_datetime(df['StudyStartTime'])
         df['StudyEndTime'] = pandas.to_datetime(df['StudyEndTime'])
+
+        # Compute study duration in minutes
         df['StudyDurationInMins'] = (df.StudyEndTime - df.StudyStartTime).dt.seconds / 60.0
 
         df = df.join(df.groupby('PatientID')['AcquisitionDateTime'].aggregate(['min', 'max']), on='PatientID')
@@ -72,6 +76,8 @@ def strip():
         df.rename(columns={'max': 'PatientEndTime'}, inplace=True)
         df['PatientStartTime'] = pandas.to_datetime(df['StudyStartTime'])
         df['PatientEndTime'] = pandas.to_datetime(df['StudyEndTime'])
+
+        # Compute patient duration in minutes
         df['PatientDurationInMins'] = (df.PatientEndTime - df.PatientStartTime).dt.seconds / 60.0
 
         df = df.join(df.groupby('DeviceSerialNumber')['AcquisitionDateTime'].aggregate(['min', 'max']),
@@ -81,6 +87,8 @@ def strip():
         df.rename(columns={'max': 'ScannerOff'}, inplace=True)
         df['ScannerOn'] = pandas.to_datetime(df['ScannerOn'])
         df['ScannerOff'] = pandas.to_datetime(df['ScannerOff'])
+
+        # Compute scanner on duration in minutes
         df['ScannerTotalOnTimeInMins'] = (df.ScannerOff - df.ScannerOn).dt.seconds / 60.0
 
         # Sort by "DeviceSerialNumber" and "SeriesStartTime"
